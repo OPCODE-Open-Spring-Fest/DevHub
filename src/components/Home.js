18153cd49fb9c64082bc1c17ac33630bdd182fde
vivek <vivekrealc2@gmail.com>
2024-03-22 13:44:16 +0530

    feat: add profile details

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from '../Navbar.js'
 import Kriti2101066 from "./developers/Kriti2101066.js";
+import Vivek2101240 from "./developers/Vivek2101240.js";
 
 const Home = () => {
   return (
     <div className="bg-white">
       <Navbar />
       <Kriti2101066 />
+      <Vivek2101240 />
     </div>
   );
 };
